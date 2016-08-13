21eebf82b8142481341d87c09f19e4dd56548d32
Nguyen Anh Quynh <aquynh@gmail.com>
2016-08-14 01:10:14 +0800

    cosmetic change

--- keypatch.py
+++ keypatch.py
@@ -1131,7 +1131,7 @@ class Keypatch_Plugin_t(idaapi.plugin_t):
             if version_stable == VERSION:
                 idc.Warning("Good, your Keypatch is already the latest stable version %s!" %VERSION)
             else:
-                idc.Warning("Your Keypatch is v%s.\nDownload stable version %s from http://keystone-engine.org/keypatch!" %(VERSION, version_stable))
+                idc.Warning("Your Keypatch is v%s.\n\nDownload stable version %s from http://keystone-engine.org/keypatch" %(VERSION, version_stable))
         else:
             # fail to download
             idc.Warning("ERROR: failed to connect to internet. Try again later.")
