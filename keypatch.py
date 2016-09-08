2f06a26a4157357619687fc695e7d32d566b438e
quangnh89 <quangnh89@gmail.com>
2016-09-08 16:17:23 +0700

    fixed: set default values if not defined in config file

--- keypatch.py
+++ keypatch.py
@@ -1366,10 +1366,15 @@ class Keypatch_Plugin_t(idaapi.plugin_t):
             print("Keypatch: exception: {0}".format(str(e)))
 
         # use default values if not defined in config file
-        self.opts['c_opt_padding'] = self.opts.get('c_opt_padding', 1)
-        self.opts['c_opt_comment'] = self.opts.get('c_opt_comment', 2)
+        if 'c_opt_padding' not in self.opts:
+            self.opts['c_opt_padding'] = 1
+
+        if 'c_opt_comment' not in self.opts:
+            self.opts['c_opt_comment'] = 2
+
         if 'c_opt_reanalyze' not in self.opts:
             self.opts['c_opt_reanalyze'] = 4
+
         self.opts['c_opt_chk'] = self.opts['c_opt_padding'] | self.opts['c_opt_comment'] | self.opts['c_opt_reanalyze']
 
     def init(self):
